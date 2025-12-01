47043d8a6719530773513670ad8410fd8bfaaadb
Hector Carrasco <mail@hcarrasco.cl>
2025-11-30 21:30:12 -0300

    feat/base-structure

---
+++ src/components/Title.tsx
@@ -0,0 +1,10 @@
+function Title() {
+  return (
+    <section className="title-wrapper">
+      <img src="/logo.png" alt="Logo" />
+      <h2>Title Component</h2>
+    </section>
+  );
+}
+
+export default Title;

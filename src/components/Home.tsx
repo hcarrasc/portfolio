cbf2d98a51af125df31dd4d4b88df5d8c84360e9
Hector Carrasco <mail@hcarrasco.cl>
2025-12-01 16:04:10 -0300

    feat/add profile section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,49 +1,46 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Title from "./Title";
-import Profile from "./Profile";
+import Header from './Header';
+import Footer from './Footer';
+import Title from './Title';
+import Profile from './Profile';
 
 function Home() {
-  return (
-    <>
-      <Header />
-      <div className="main-wrapper">
-        <Profile />
-        <Title />
-        <section className="side-projects">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, nam! Et
-          illo ut facere, magni aspernatur ad perspiciatis ducimus vero veniam
-          eveniet, nam perferendis dolores, voluptas dolore optio culpa facilis.
-        </section>
+    return (
+        <>
+            <Header />
+            <div className="main-wrapper">
+                <Profile />
+                <Title />
+                <section className="side-projects">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, nam! Et illo ut
+                    facere, magni aspernatur ad perspiciatis ducimus vero veniam eveniet, nam
+                    perferendis dolores, voluptas dolore optio culpa facilis.
+                </section>
 
-        <Title />
-        <section className="stack">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id rem quasi
-          nesciunt! Provident labore laborum ad autem nisi quibusdam sit, amet
-          placeat accusamus voluptatem praesentium ea dolores, quo atque
-          dolorum!
-        </section>
+                <Title />
+                <section className="stack">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id rem quasi nesciunt!
+                    Provident labore laborum ad autem nisi quibusdam sit, amet placeat accusamus
+                    voluptatem praesentium ea dolores, quo atque dolorum!
+                </section>
 
-        <Title />
-        <section className="cv">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, et
-          eius? Quaerat corrupti exercitationem, ipsum sit quod eligendi eos
-          libero dolores, alias neque nesciunt tenetur iure tempora nulla
-          inventore aperiam.
-        </section>
+                <Title />
+                <section className="cv">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, et eius? Quaerat
+                    corrupti exercitationem, ipsum sit quod eligendi eos libero dolores, alias neque
+                    nesciunt tenetur iure tempora nulla inventore aperiam.
+                </section>
 
-        <Title />
-        <section className="courses">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex
-          explicabo nisi exercitationem ipsa numquam porro vero, amet esse
-          provident facilis totam placeat! Expedita illum sed culpa esse
-          repellat ipsa.
-        </section>
-      </div>
+                <Title />
+                <section className="courses">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex explicabo
+                    nisi exercitationem ipsa numquam porro vero, amet esse provident facilis totam
+                    placeat! Expedita illum sed culpa esse repellat ipsa.
+                </section>
+            </div>
 
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,50 @@
+import img_de_perfil from '../assets/img_de_perfil.jpeg';
+import iconlinkedin from '../assets/iconlinkedin.png';
+import iconinstagram from '../assets/iconinstagram.png';
+import iconx from '../assets/iconx.png';
+import icongithub from '../assets/icongithub.png';
+
 function Profile() {
-  return <h1>Profile</h1>;
+    function handleContactme() {
+        const email = 'htcarrasc@gmail.com';
+        const subject = 'Consulta desde el sitio';
+        const body = 'Hola, me gustaría agendar un llamada contigo.';
+
+        const mailto = `mailto:${email}?subject=${encodeURIComponent(
+            subject,
+        )}&body=${encodeURIComponent(body)}`;
+        window.location.href = mailto;
+    }
+
+    return (
+        <section>
+            <div className="profile-wrapper">
+                <div className="section-img">
+                    <img src={img_de_perfil} alt="Profile Picture" />
+                    <div className="profile-location"> Concepción, Chile</div>
+                    <div className="profile-connect">
+                        <img src={iconlinkedin} alt="LinkedIn Icon" />
+                        <img src={icongithub} alt="GitHub Icon" />
+                        <img src={iconinstagram} alt="Instagram Icon" />
+                        <img src={iconx} alt="X Icon" />
+                    </div>
+                    <button className="btn-contact" onClick={handleContactme}>
+                        Contáctame o agenda una llamada
+                    </button>
+                </div>
+                <div className="section-info">
+                    <h1>Héctor Carrasco</h1>
+                    <h2>Full stack developer</h2>
+                    <p>
+                        Soy un desarrollador full stack con experiencia en la creación de
+                        aplicaciones web. Me apasiona aprender nuevas tecnologías y mejorar mis
+                        habilidades constantemente. Me gusta contribuir a proyectos innovadores y
+                        trabajar en equipo.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Profile;

60aee7889936af3a3538c2207422f0a2fdcc7214
Hector Carrasco <mail@hcarrasco.cl>
2025-12-02 10:23:04 -0300

    feat/title component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,12 @@ import Header from './Header';
 import Footer from './Footer';
 import Title from './Title';
 import Profile from './Profile';
+import box from '../assets/box.png';
+import layers from '../assets/layers.png';
+import edu from '../assets/edu.png';
+import cv from '../assets/cv.png';
+import cursos from '../assets/cursos.png';
+import Project from './Project';
 
 function Home() {
     return (
@@ -9,28 +15,33 @@ function Home() {
             <Header />
             <div className="main-wrapper">
                 <Profile />
-                <Title />
+                <Title icon={box} titleText="Proyectos personales" />
                 <section className="side-projects">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo, nam! Et illo ut
                     facere, magni aspernatur ad perspiciatis ducimus vero veniam eveniet, nam
                     perferendis dolores, voluptas dolore optio culpa facilis.
                 </section>
 
-                <Title />
-                <section className="stack">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id rem quasi nesciunt!
-                    Provident labore laborum ad autem nisi quibusdam sit, amet placeat accusamus
-                    voluptatem praesentium ea dolores, quo atque dolorum!
+                <Title icon={layers} titleText="Tech Stack" />
+                <section className="stack-section">
+                    <Project />
                 </section>
 
-                <Title />
+                <Title icon={edu} titleText="Educación" />
                 <section className="cv">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, et eius? Quaerat
                     corrupti exercitationem, ipsum sit quod eligendi eos libero dolores, alias neque
                     nesciunt tenetur iure tempora nulla inventore aperiam.
                 </section>
 
-                <Title />
+                <Title icon={cv} titleText="CV" />
+                <section className="cv">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, et eius? Quaerat
+                    corrupti exercitationem, ipsum sit quod eligendi eos libero dolores, alias neque
+                    nesciunt tenetur iure tempora nulla inventore aperiam.
+                </section>
+
+                <Title icon={cursos} titleText="Courses" />
                 <section className="courses">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae ex explicabo
                     nisi exercitationem ipsa numquam porro vero, amet esse provident facilis totam
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,15 @@
-function Title() {
-  return (
-    <section className="title-wrapper">
-      <img src="/logo.png" alt="Logo" />
-      <h2>Title Component</h2>
-    </section>
-  );
+type TitleProps = {
+    icon: string;
+    titleText: string;
+};
+
+function Title({ icon, titleText }: TitleProps) {
+    return (
+        <section className="title-wrapper">
+            <img src={icon} alt="Logo" />
+            <h2>{titleText}</h2>
+        </section>
+    );
 }
 
 export default Title;
